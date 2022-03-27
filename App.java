68b7b057b887e695eaa3be78f1c2761f9391a466
Lucas <LucasClavel356@gmail.com>
2022-03-27 22:41:03 +0200

    Seting up medewerker

---
+++ App.java
@@ -0,0 +1,13 @@
+import javax.sql.rowset.spi.SyncResolver;
+
+public class App {
+
+    public static void main( String[] args){
+        hoi();
+    }
+
+    public static void hoi(){
+    System.out.println("Hoi");
+    }
+
+}
--- Medewerker.java
+++ Medewerker.java
@@ -1,1 +1,31 @@
+public class Medewerker {
+    String Vnaam;
+    String Anaam;
+    int werkTijd;
+    
+    /*
+    int padInter;
+    int padPotjes;
+    int padFris;
+    int padBier;
+    int padChips;
+    int padWijn;
+    int padCosmetica;
+    int padDierenvoeding;
+    int padKoek;
+    int padOntbijt;
+    int padDiepvries;
+    int padZuivel;
+    int padVVP;
+    */
 
+    public Medewerker(String Vnaam, String Anaam, int werkTijd){
+            this.Vnaam = Vnaam;
+            this.Anaam = Anaam;
+            this.werkTijd = werkTijd;
+    }
+
+    public String getNaam(){
+        return this.Vnaam + this.Anaam;
+    }   
+}
