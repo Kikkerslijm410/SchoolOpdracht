6d065c5596efe1d07e34ee7bf93b5e5e41ba2d6f
Lucas <LucasClavel356@gmail.com>
2022-03-28 13:17:35 +0200

    Joe Joe

--- Medewerker.java
+++ Medewerker.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Medewerker {
-    String Vnaam;
-    String Anaam;
+    String naam;
     int werkTijd;
+    public static ArrayList<Medewerker> MedewerkerList;
     
     /*
+    ff hiernaar kijken hoe dit het beste aangegeven kan worden misschien aparte class
     int padInter;
     int padPotjes;
     int padFris;
@@ -19,13 +23,39 @@ public class Medewerker {
     int padVVP;
     */
 
-    public Medewerker(String Vnaam, String Anaam, int werkTijd){
-            this.Vnaam = Vnaam;
-            this.Anaam = Anaam;
+    public Medewerker(String naam, int werkTijd){
+            this.naam = naam;
             this.werkTijd = werkTijd;
+            MedewerkerList.add(this);
+    }
+
+    public void setWerkTijd(int werkTijd){
+        this.werkTijd = werkTijd;
     }
 
     public String getNaam(){
-        return this.Vnaam + this.Anaam;
-    }   
+        return this.naam;
+    }
+
+    public static void newStudent(Scanner scanner){
+        try{
+            
+            System.out.println ("Voer de voor- en achternaam in:");
+            String naam = scanner.nextLine();
+            naam = naam.replace("\n", "");
+            System.out.println("Voer de werktijd in:");
+            int nummer = scanner.nextInt();
+            scanner.nextLine();
+            Medewerker medewerker = new Medewerker(naam, nummer);
+            System.out.println(medewerker.getNaam() + " is toegevoegd aan de medewerker lijst.");
+            System.out.println("Press return to continue");
+            scanner.nextLine();
+            
+        }
+        catch(Exception e){
+            System.out.println("Medewerker kon niet toegevoegd worden");
+            System.out.println(e);
+        }
+    }
+
 }
---
+++ Vulplanning.java
@@ -0,0 +1,26 @@
+public class Vulplanning {
+
+    public static void main( String[] args){
+        Start();
+        medewerkerAdd();
+    }
+
+    public static void Start(){
+        
+    System.out.println(" ");
+    }
+
+    public void maakPlanning(){
+
+    }
+
+    public void medewerkerDelete(){
+
+    }
+    
+    public static void medewerkerAdd(){
+        Medewerker medewerker1 = new Medewerker("Lucas Clavel", 270);
+        Medewerker medewerker2 = new Medewerker("Lucas Clavel", 270);
+
+    }
+}
