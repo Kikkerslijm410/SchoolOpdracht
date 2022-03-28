2742e51b79168cea881ec54ddfda0744f65ba49a
Lucas <LucasClavel356@gmail.com>
2022-03-28 14:44:39 +0200

    kaaskoekje

--- Medewerker.java
+++ Medewerker.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Medewerker {
     String naam;
     int werkTijd;
-    public static ArrayList<Medewerker> MedewerkerList;
+    static ArrayList<Medewerker> MedewerkerList;
     
     /*
     ff hiernaar kijken hoe dit het beste aangegeven kan worden misschien aparte class
@@ -37,9 +37,8 @@ public class Medewerker {
         return this.naam;
     }
 
-    public static void newStudent(Scanner scanner){
+    public static void medewerkerAdd(Scanner scanner){
         try{
-            
             System.out.println ("Voer de voor- en achternaam in:");
             String naam = scanner.nextLine();
             naam = naam.replace("\n", "");
@@ -50,10 +49,30 @@ public class Medewerker {
             System.out.println(medewerker.getNaam() + " is toegevoegd aan de medewerker lijst.");
             System.out.println("Press return to continue");
             scanner.nextLine();
-            
         }
         catch(Exception e){
             System.out.println("Medewerker kon niet toegevoegd worden");
+            System.out.println(e);
+        }
+    }
+
+    public static void medewerkerDelete(Scanner scanner){
+        try{
+            int counter = 1;
+            for (Medewerker e : Medewerker.MedewerkerList){
+                System.out.println(counter+") "+e.getNaam());
+                counter++;
+            }
+            System.out.println("Kies een medewerker:");
+            int userRemoveStudentChoice = scanner.nextInt();
+            scanner.nextLine();
+            Medewerker.MedewerkerList.remove(userRemoveStudentChoice-1);
+            System.out.println("Medewerker succesvol verwijderd");
+            System.out.println("druk op enter om terug te gaan naar het menu");
+            scanner.nextLine();
+        }
+        catch(Exception e){
+            System.out.println("Medewerker bestaat niet.");
             System.out.println(e);
         }
     }
--- Vulplanning.java
+++ Vulplanning.java
@@ -1,26 +1,105 @@
+import java.io.IOException;
+//import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Vulplanning {
 
+    private static final Scanner Scanner = null;
+
     public static void main( String[] args){
-        Start();
-        medewerkerAdd();
+        Medewerker.medewerkerAdd(Scanner);
+        Scanner scanner = new Scanner(System.in);
+        Medewerker medewerker = new Medewerker("Lucas Clavel", 400);
+        mainMenu(scanner);
     }
 
-    public static void Start(){
-        
-    System.out.println(" ");
-    }
+        //mainMenu
+        public static void mainMenu(Scanner james) {
+            clearScreen();
+            mainMenuLoop: while (true) {
+                printMainMenu();
+                int chooseAction = james.nextInt();
+                james.nextLine();
+                try {
+                    switch (chooseAction) {
+                        case (1):
+                            clearScreen();
+                            maakPlanning();
+                            clearScreen();
+                            break;
+                        case (2):
+                            clearScreen();
+                            getMedewerkers(james);
+                            clearScreen();
+                            break;
+                        case (3):
+                            clearScreen();
+                            Medewerker.medewerkerAdd(james);
+                            break;
+                        case (4):
+                            clearScreen();
+                            Medewerker.medewerkerDelete(james);
+                            break;
+                        case (5):
+                            clearScreen();
+                            
+                            break;
+                        case (6):
+                            clearScreen();
+                            
+                            break;
+                        case (7):
+                            clearScreen();
+                            
+                            break;
+                        case (8):
+                            clearScreen();
+                            ;
+                            break;
+                        case(0):
+                            System.out.println("Saving Data.....");
+                            break mainMenuLoop;
+                        default:
+                            System.out.println("Optie bestaat niet. Kies een juiste optie uit de lijst");
+                            break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error in the mainMenu method!");
+                    System.out.println(e);
+                }
+            }
+        }
 
-    public void maakPlanning(){
-
-    }
-
-    public void medewerkerDelete(){
-
-    }
+        // clearScreen
+        public static void clearScreen() {
+            try {
+                if (System.getProperty("os.name").contains("Windows"))
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                else
+                    Runtime.getRuntime().exec("clear");
+            } catch (IOException | InterruptedException ex) {}
+        }
     
-    public static void medewerkerAdd(){
-        Medewerker medewerker1 = new Medewerker("Lucas Clavel", 270);
-        Medewerker medewerker2 = new Medewerker("Lucas Clavel", 270);
+        // printMainMenu
+        private static void printMainMenu() {
+            System.out.println("1) Maak de planning");
+            System.out.println("2) Lijst met medewerkers");
+            System.out.println("3) Nieuwe medewerker aanmaken");
+            System.out.println("4) Medewerker verwijderen");
+            System.out.println("5) ");
+            System.out.println("6) "); 
+            System.out.println("7) ");
+            System.out.println("8) ");
+            System.out.println("0) Exit");
+        }
+        
+        private static void maakPlanning() {
 
-    }
+        }
+
+        private static void getMedewerkers(Scanner james) {
+            for (Medewerker e : Medewerker.MedewerkerList) {
+                System.out.println(e.getNaam());
+            }
+        }
 }
