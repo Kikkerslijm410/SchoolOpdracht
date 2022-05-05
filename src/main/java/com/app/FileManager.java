eefec1643e8ed2bac1afb038d35645a177fdee36
Lucas <LucasClavel356@gmail.com>
2022-05-05 14:05:07 +0200

    Commit 69

---
+++ src/main/java/com/app/FileManager.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.poi.xssf.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class FileManager {
+
+    public void Start(){
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+    }
+}
