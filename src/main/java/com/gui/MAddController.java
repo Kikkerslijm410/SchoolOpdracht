bb427f2a382274ddb248b8d3350aa0fb6fc14f72
Lucas <LucasClavel356@gmail.com>
2022-06-06 14:46:43 +0200

    bug fixes

--- src/main/java/com/gui/MAddController.java
+++ src/main/java/com/gui/MAddController.java
@@ -2,19 +2,31 @@ package com.gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.app.GsonManager;
+import com.app.LeaderBoard;
 import com.app.Medewerker;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class MAddController extends AController implements Initializable{
+
+    private List<Medewerker> users = new ArrayList<>();
+    
+    Medewerker medewerker;
 
     @FXML
     private TextField naam;
@@ -25,7 +37,17 @@ public class MAddController extends AController implements Initializable{
     @FXML
     private Label MedewerkerNaam;
 
-    Medewerker medewerker;
+    @FXML
+    private TableView<Medewerker> leaderboard;
+
+    @FXML
+    private final TableColumn<Object, Object> rankColumn = new TableColumn<>();
+
+    @FXML
+    private final TableColumn<Object, Object> namesKolom = new TableColumn<>();
+
+    @FXML
+    private final TableColumn<Object, Object> werktijdColumn = new TableColumn<>();
 
     //switch statements
     @FXML
@@ -89,6 +111,12 @@ public class MAddController extends AController implements Initializable{
     public void initialize(URL url, ResourceBundle resourceBundle) {
         addNumberLimiter();
         addTextLimiter(3);
+        users = LeaderBoard.getUsers();
+        rankColumn.setCellValueFactory(new PropertyValueFactory<>("rank"));
+        namesKolom.setCellValueFactory(new PropertyValueFactory<>("naam"));
+        werktijdColumn.setCellValueFactory(new PropertyValueFactory<>("werktijd"));
+        ObservableList<Medewerker> data = FXCollections.observableArrayList(users);
+        leaderboard.setItems(data);
     }
 
     //Override just here because implementations
