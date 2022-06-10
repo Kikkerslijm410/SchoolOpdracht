777f43600f9a7617d13870afe76c05fcd0a84b80
Lucas <LucasClavel356@gmail.com>
2022-06-10 14:03:30 +0200

    oepsie

--- src/main/java/com/app/ExcelManager.java
+++ src/main/java/com/app/ExcelManager.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.TreeMap;
 
 import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
@@ -22,24 +23,39 @@ public abstract class ExcelManager {
         // creating a row object
         XSSFRow row;
         // This data needs to be written (Object[])
-        Map<String, Object[]> Vulplanning = new TreeMap<>();
+        Map<String, Object[]> Vulplanning = new TreeMap<String, Object[]>();
         Vulplanning.put("1", new Object[] {});
         Vulplanning.put("2", new Object[] { "", "Totale:", "Vultijd:", vultijdTotaal(), "Vracht:", vrachtTotaal()  });
-        Vulplanning.put("3", new Object[] { "", "Medewerkers:", "Paden", "Vracht", "Vultijd" });
-  
-        Vulplanning.put("4", new Object[] { "130", "Mohan", "2nd year" });
-  
-        Vulplanning.put("5", new Object[] { "131", "Radha", "2nd year" });
+        Vulplanning.put("3", new Object[] { "", "Ploeg:", "Paden", "Vracht", "Vultijd" });
+        Vulplanning.put("4", new Object[] {"", "", "", "Internationaal"});
+        Vulplanning.put("5", new Object[] {"", "", "", "Potjes"});
+        Vulplanning.put("6", new Object[] {"", "", "", "Frisdrank"});
+        Vulplanning.put("7", new Object[] {"", "", "", "Bier"});
+        Vulplanning.put("8", new Object[] {"", "", "", "Wijn"});
+        Vulplanning.put("9", new Object[] {"", "", "", "Chips"});
+        Vulplanning.put("10", new Object[] {"", "", "", "Cosmetica"});
+        Vulplanning.put("11", new Object[] {"", "", "", "Dierenvoeding"});
+        Vulplanning.put("12", new Object[] {"", "", "", "Koek"});
+        Vulplanning.put("13", new Object[] {"", "", "", "Ontbijt"});
+        Vulplanning.put("14", new Object[] {"", "", "", "Zuivel"});
+        Vulplanning.put("15", new Object[] {"", "", "", "VVP"});
+        Vulplanning.put("16", new Object[] {"", "", "", "Diepvries"});
+        Vulplanning.put("17", new Object[] {});
+        Vulplanning.put("18", new Object[] {});
+        Vulplanning.put("19", new Object[] {});
+        Vulplanning.put("20", new Object[] {});
+        Vulplanning.put("21", new Object[] {});
+        Vulplanning.put("22", new Object[] {});
+        Vulplanning.put("23", new Object[] {});
+        Vulplanning.put("24", new Object[] {});
+        Vulplanning.put("25", new Object[] {});
+        Vulplanning.put("26", new Object[] {});
+        Vulplanning.put("27", new Object[] {});
+        Vulplanning.put("28", new Object[] {});
 
-        Vulplanning.put("6", new Object[] { "hallo", "", "" });
-
-        Vulplanning.put("7", new Object[] { "hallo dit is een nieuwe zin", "Gopal", "2nd year" });
-  
-  
         Set<String> keyid = Vulplanning.keySet();
   
         int rowid = 0;
-  
         // writing the data into the sheets...
         for (String key : keyid) {
             row = spreadsheet.createRow(rowid++);
@@ -55,23 +71,24 @@ public abstract class ExcelManager {
         FileOutputStream out = new FileOutputStream(new File(dir+"Vulplanning.xlsx"));
         workbook.write(out);
         out.close();
+        workbook.close();
     }
 
-    public static int vultijdTotaal (){
+    public static String vultijdTotaal (){
         int counter = 0;
         for (Pad e : Pad.PadList){
             int i = e.getAantalDozen() / e.getVulnorm() * 60;
             counter += i;
         }
-        return counter;
+        return "" + counter;
     }
 
-    public static int vrachtTotaal (){
+    public static String vrachtTotaal (){
         int counter = 0;
         for (Pad e : Pad.PadList){
             counter += e.getAantalDozen();
         }
-        return counter;
+        return "" + counter;
     }
 
     public static String vultijd (String padNaam){
